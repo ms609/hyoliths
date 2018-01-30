17839ce7899b4ff59ac49a331ffb46afea5b583d
ms609 <martins@gmail.com>
2018-01-30 16:50:19 +0000

    verbose; consensu

--- HyTreeSearch.R
+++ HyTreeSearch.R
@@ -23,11 +23,11 @@ best.tree <- Ratchet(better.tree, my_data, verbosity=0, k=5,
 for (i in 1:10) {
   plot(best.tree <- Ratchet(best.tree, my_data, verbosity=0,
                        swappers=list(RootedTBRSwap, RootedSPRSwap, RootedNNISwap)))
-  text(0.5, 0.5, paste0("Score: ", Fitch(best.tree, my_data)), pos=4)
+  text(0.5, 0.5, paste0("Search iteration: ", i, "; Score: ", Fitch(best.tree, my_data)), pos=4)
 }
 
 
-my.consensus <- RatchetConsensus(best.tree, my.phyDat, swappers=
+my.consensus <- RatchetConsensus(best.tree, my_data, ratchHits = 100, nSearch=100, swappers=
                                  list(RootedTBRSwap, RootedNNISwap)) 
 par(mar=rep(0.25, 4), cex=0.75) # make plot easier to read
 plot(ape::consensus(my.consensus))                               
