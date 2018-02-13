e4f5e9f38004112123917a0672240203e7234869
ms609 <martins@gmail.com>
2018-02-13 10:46:48 +0000

    paths and links to iw_data not my_data

--- HyTreeSearch.R
+++ HyTreeSearch.R
@@ -1,6 +1,6 @@
 library('TreeSearch'); library('ape'); library('phangorn')
 library('Inapp') #  For parse.nexus
-filename <- "X24932.nex"
+filename <- "../Hyoliths/X24932.nex"
 # First replace all spaces in taxon names with underscores, remove ''s, and replace
 # polymorphisms with "?".
 #raw_data <- ape::read.nexus.data(filename)
@@ -45,10 +45,11 @@ iw.tree <- IWTreeSearch(tree=iw.tree, dataset=iw_data, concavity=k,
                            EdgeSwapper=RootedNNISwap, verbosity=3)
 for (i in 1:100) {
   cat("\n\n############################# I =", i)
-  plot(iw.tree <- IWRatchet(iw.tree, my_data, verbosity=2, concavity=k,
+  plot(iw.tree <- IWRatchet(iw.tree, iw_data, verbosity=2, concavity=k,
                        swappers=list(RootedTBRSwap, RootedSPRSwap, RootedNNISwap)))
   text(0.5, 0.5, paste0("IW Score: ", IWScore(iw.tree, iw_data, concavity=k)), pos=4)
 }
+write.nexus(iw.tree, file=paste0("../Hyoliths/hy_iw_k4_", round(IWScore(iw.tree, iw_data, concavity=k), 3), ".tre", collapse=''))
 
 iw.consensus <- IWRatchetConsensus(iw.tree, iw_data, 
                 swappers=list(RootedTBRSwap, RootedNNISwap),
@@ -56,10 +57,9 @@ iw.consensus <- IWRatchetConsensus(iw.tree, iw_data,
                 nSearch=20) 
 par(mar=rep(0.25, 4), cex=0.75) # make plot easier to read
 plot(ape::consensus(iw.consensus))                               
-write.nexus(iw.tree, file=paste0("hy_iw_k4_", round(IWScore(iw.tree, iw_data, concavity=k), 3), ".tre", collapse=''))
-write.nexus(iw.consensus, file=paste0("hy_iw_k4_", round(IWScore(iw.tree, iw_data, concavity=k), 3), ".con.tre", collapse=''))
+write.nexus(iw.consensus, file=paste0("../Hyoliths/hy_iw_k4_", round(IWScore(iw.tree, iw_data, concavity=k), 3), ".con.tre", collapse=''))
 
-pdf(file=paste0("hy_iw_", round(IWScore(iw.tree, my_data, concavity=k), 3), ".pdf", collapse=''))
+pdf(file=paste0("../Hyoliths/hy_iw_", round(IWScore(iw.tree, my_data, concavity=k), 3), ".pdf", collapse=''))
 par(mar=rep(0.25, 4), cex=0.75) # make plot easier to read
 plot(ape::consensus(iw.consensus))
 dev.off()
