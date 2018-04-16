e902531313242a7d0f7f85df483eb3dc68697cf0
ms609 <martins@gmail.com>
2018-04-16 14:41:25 +0100

    Don't suggest or pre-install TreeSearch

--- _build.sh
+++ _build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-Rscript -e "install.packages('TreeSearch')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
