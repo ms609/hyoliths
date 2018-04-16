800e0b03a68929ccfa4db583567139f3f3e5ac54
ms609 <martins@gmail.com>
2018-04-16 14:19:48 +0100

    Install from CRAN not GitHub

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Rscript -e "devtools::install_github('ms609/TreeSearch')"
+Rscript -e "install.packages('TreeSearch')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
