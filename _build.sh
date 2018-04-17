370d7d71e46db47fa2adf1c35a7dc219c892ddac
ms609 <martins@gmail.com>
2018-04-17 14:45:18 +0100

    Try installing Garamond again

--- _build.sh
+++ _build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #tlmgr install collection-fontsrecommended
-#tlmgr install collection-fontsextra
+tlmgr install ebgaramond
 Rscript -e "devtools::install_github('ropensci/rcrossref')" # Need version 0.8.1 or higher
 Rscript -e " rmarkdown::render_site(encoding = 'UTF-8')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
