483420a338ff07fccd548a2ea9f4130ff4d8f203
ms609 <martins@gmail.com>
2018-04-17 13:49:35 +0100

    rmarkdown::render_site in place of bookdown::render_book

--- _build.sh
+++ _build.sh
@@ -2,8 +2,8 @@
 
 tlmgr install collection-fontsrecommended
 tlmgr install collection-fontsextra
-Rscript -e "devtools::install_github('ropensci/rcrossref')"
-Rscript -e "bookdown::render_book('index.Rmd')"
+Rscript -e "devtools::install_github('ropensci/rcrossref')" # Need version 0.8.1 or higher
+Rscript -e " rmarkdown::render_site(encoding = 'UTF-8')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
