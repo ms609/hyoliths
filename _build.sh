66254ef2bfbc63ecf8c4fbc7707f32d68aab0184
ms609 <martins@gmail.com>
2018-04-17 13:33:55 +0100

    Does this do a 3-way render?

--- _build.sh
+++ _build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 Rscript -e "devtools::install_github('ropensci/rcrossref')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+Rscript -e "bookdown::render_book('index.Rmd')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
