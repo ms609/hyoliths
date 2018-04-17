c696bda0e897820b438de8ca00c47390db03b57e
ms609 <martins@gmail.com>
2018-04-17 11:41:45 +0100

    Will updating rcrossref fix errors?
    They don't occur locally.

--- _build.sh
+++ _build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+Rscript -e "devtools::install_github('ropensci/rcrossref')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
