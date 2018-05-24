688fc33795beddddb3dbf03f0c893f6e16a1621b
Martin Smith <martins@gmail.com>
2018-05-24 12:57:02 +0100

    Travis wait where it needs to be

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 
 Rscript -e "devtools::install_github('ropensci/rcrossref')" # Need version 0.8.1 or higher
 Rscript -e "devtools::install_github('ms609/TreeSearch')" # Need version > 0.1.2
-travis_wait 45 Rscript -e " rmarkdown::render_site(encoding = 'UTF-8')"
+Rscript -e " rmarkdown::render_site(encoding = 'UTF-8')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
