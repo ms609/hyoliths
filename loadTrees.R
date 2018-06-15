aca96237c2eb230742fd6a2767ceccb35f523497
Martin Smith <martins@gmail.com>
2018-06-15 11:50:37 +0100

    Failing to find globally best trees.  Improve
    Not 100% happy about jumping to another k value; could this narrow the final range of trees sampled?

---
+++ loadTrees.R
@@ -0,0 +1,24 @@
+ew.best <- list.files('TreeSearch', pattern='hy_ew_\\d*\\.nex', full.names=TRUE)
+ew.trees <- read.nexus(file=ew.best[which.max(file.mtime(ew.best))])
+ew.trees <- if (class(ew.trees) == 'multiPhylo') unique(ew.trees) else ew.trees
+ew.trees <- as.multiPhylo(ew.trees)
+iw.trees <- lapply(kValues, function (k) {
+  iw.best <- list.files('TreeSearch',
+                        pattern=paste0('hy_iw_k',
+                                       gsub('\\.', '\\\\.', k),
+                                       '_\\d+\\.?\\d*\\.all\\.nex'),
+                        full.names=TRUE)
+  # Return:
+  if (length(iw.best) == 0) {
+    list()
+  } else {
+    loadedTrees <- read.nexus(iw.best[which.max(file.mtime(iw.best))])
+    if (class(loadedTrees) == 'multiPhylo') unique(loadedTrees) else as.multiPhylo(loadedTrees)
+  }
+})
+iw.exist <- iw.trees[vapply(iw.trees, length, 0) > 0]
+
+allTrees <- ew.trees
+for (i in seq_along(iw.exist)) {
+  allTrees <- c(allTrees, iw.exist[[i]])
+}
