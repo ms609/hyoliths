306bc02a2e6ea199a2493ea87580b0eb12987b83
Martin Smith <martins@gmail.com>
2018-06-22 09:50:20 +0100

    Rm duplicated function

--- loadTrees.R
+++ loadTrees.R
@@ -1,39 +1,3 @@
-FormatTree <- function(tree) {
-  edge <- tree$edge
-  parent <- edge[, 1]
-  child <- edge[, 2]
-  tipLabels <- tree$tip.label
-  tree.ntip <- length(tipLabels)
-  descendants <- Descendants(tree)
-  nDescendants <- vapply(descendants, length, integer(1))
-  MinKid <- function (tips) min(tipLabels[tips])
-  swaps <- vapply(tree.ntip + 1:Nnode(tree), function(node) {
-    kids <- child[parent == node]
-    descs <- nDescendants[kids]
-    if (all(descs == 1L)) {
-      order(tipLabels[kids])[1] == 1
-    } else if (descs[1] == descs[2]) {
-      order(vapply(descendants[kids], MinKid, character(1)))[1] == 1
-    } else {
-      descs[1] < descs[2]
-    }
-  }, logical(1))
-  for (node in tree.ntip + rev(which(swaps))) {
-    childEdges <- parent==node
-    kids <- child[childEdges]
-    child[childEdges][2:1] <- kids
-  }
-  #tree2 <- tree
-  #tree2$edge <- cbind(parent, child)
-  #dev.new(); par(mfrow=c(2, 1), cex=0.75, mar=rep(0.2, 4))
-  #plot(tree); nodelabels(tree.ntip + rev(which(swaps)), node=tree.ntip + rev(which(swaps)), bg='green')
-  #plot(tree2); nodelabels(tree.ntip + rev(which(swaps)), node=tree.ntip + rev(which(swaps)), bg='green')
-  #tree2
-  tree$edge[, 1] <- parent
-  tree$edge[, 2] <- child
-  tree
-}
-
 ew.best <- list.files('TreeSearch', pattern='hy_ew_\\d*\\.nex', full.names=TRUE)
 ew.trees <- read.nexus(file=ew.best[which.max(file.mtime(ew.best))])
 ew.trees <- if (class(ew.trees) == 'multiPhylo') unique(ew.trees) else ew.trees
