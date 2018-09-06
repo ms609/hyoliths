93a291880299e9fe16aa4b139a73a70f228601c3
Martin Smith <martins@gmail.com>
2018-09-06 12:18:36 +0100

    Just build HTML for now

--- _build.sh
+++ _build.sh
@@ -4,5 +4,5 @@ Rscript -e "devtools::install_github('ropensci/rcrossref')" # Need version 0.8.1
 Rscript -e "devtools::install_github('ms609/TreeSearch')" # Need version > 0.1.2
 #Rscript -e " rmarkdown::render_site(encoding = 'UTF-8')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
