3e321b91ad0dc0cdbafea9da0b446a65a52a75ba
Martin Smith <martins@gmail.com>
2018-09-06 12:23:16 +0100

    Specify version requirements; use CRAN where available

--- _build.sh
+++ _build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-Rscript -e "devtools::install_github('ropensci/rcrossref')" # Need version 0.8.1 or higher
 Rscript -e "devtools::install_github('ms609/TreeSearch')" # Need version > 0.1.2
 #Rscript -e " rmarkdown::render_site(encoding = 'UTF-8')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
