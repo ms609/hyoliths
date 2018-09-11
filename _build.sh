2c340e90e5b7e9b7adde2e3ea3541a31493bebaa
Martin Smith <martins@gmail.com>
2018-09-11 13:54:05 +0100

    Build gitbook

--- _build.sh
+++ _build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 #Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
