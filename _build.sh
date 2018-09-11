77116f170e0402fda9063a1cf858d6e5324dc1a9
Martin Smith <martins@gmail.com>
2018-09-11 11:09:11 +0100

    Build PDF book

--- _build.sh
+++ _build.sh
@@ -2,6 +2,6 @@
 
 Rscript -e "devtools::install_github('ms609/TreeSearch')" # Need version > 0.1.2
 #Rscript -e " rmarkdown::render_site(encoding = 'UTF-8')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+#Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
