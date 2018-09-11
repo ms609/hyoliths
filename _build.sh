6c7ebe56d2eaf60c857c98af7b1c6f06b1c6472f
Martin Smith <martins@gmail.com>
2018-09-11 12:11:41 +0100

    TreeSearch 0.2.0 now on CRAN
    Remove github remote links

--- _build.sh
+++ _build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-Rscript -e "devtools::install_github('ms609/TreeSearch')" # Need version > 0.1.2
 Rscript -e "rmarkdown::render_site(encoding = 'UTF-8')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 #Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
